675073bfd950a411d7968d0dee43096a605817a5
OswaldoGSDAW <147555767+OswaldoGSDAW@users.noreply.github.com>
2024-01-16 23:41:46 +0100

    Error de variables arreglado Cuando entras y no tiene valor user y pass daba warnings, con lo cambiado no los da

--- controller/formLogin.php
+++ controller/formLogin.php
@@ -3,8 +3,16 @@
 require 'model/Login.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+    /*
+    El operador ?? es el operador de fusión de null en PHP. Este operador devuelve su primer operando si existe y no es NULL; 
+    de lo contrario, devuelve su segundo operando. En este caso, si $_POST['username'] o $_POST['password'] existen y no son NULL, 
+    sus valores se asignarán a las variables $username y $password respectivamente. 
+    Si no existen o son NULL, se asignará una cadena vacía ('') a las variables.
+    Esto es útil para evitar errores cuando se intenta acceder a índices que no existen en un array, 
+    lo cual ocurriría si se intenta acceder a $_POST['username'] o $_POST['password'] cuando no se han enviado datos a través del método POST.
+    */
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['password'] ?? '';
 
     $loginModel = new Login();
     if ($loginModel->comprobarCredenciales($username, $password)) {
